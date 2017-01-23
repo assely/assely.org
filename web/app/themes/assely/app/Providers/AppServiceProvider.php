600b618f54d67f2a7d9e873f8750f1d851da4c04
jedrzejchalubek <jedrzej.chalubek@gmail.com>
2017-01-23 01:40:45 +0100

    Updates according to the changes in Support component

--- web/app/themes/assely/app/Providers/AppServiceProvider.php
+++ web/app/themes/assely/app/Providers/AppServiceProvider.php
@@ -2,8 +2,8 @@
 
 namespace App\Providers;
 
-use Assely\Support\Facades\Hook;
-use Assely\Support\Support;
+use Hook;
+use Support;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
